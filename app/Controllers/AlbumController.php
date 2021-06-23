0c890c4003024295cb8c6ed3241cb550dcba1b64
Henry Li <henry_li@web.de>
2021-06-24 00:14:40 +0100

    Code modularisation

--- app/Controllers/AlbumController.php
+++ app/Controllers/AlbumController.php
@@ -9,65 +9,74 @@ use Flight;
 
 class AlbumController extends Controller {
 	public function index($albumId) {
-		if (str_starts_with($albumId, DeezerApi::DEEZER_ID_PREFIX)) {
-			$albumId = str_replace(DeezerApi::DEEZER_ID_PREFIX, "", $albumId);
-			$filepath = "userData/deezer/album/$albumId";
-	
-			if (!file_exists($filepath)) {
-				$deezerApi = new DeezerApi();
-				$res = $deezerApi->getAlbum($albumId);
-				
-				file_put_contents($filepath, serialize($res));
-			} else {
-				$res = unserialize(file_get_contents($filepath));
-			}
-	
-			$album = $res['album'];
-			$songs = $res['songs'];
-	
-			$albumArt = file_get_contents($album['artUrl']);
-		} else {
-			$db = new MusicDatabase();
-			$conn = $db->getConn();
+		$data = str_starts_with($albumId, DeezerApi::DEEZER_ID_PREFIX) ? $this->getDeezerAlbum($albumId) : $this->getLocalAlbum($albumId);
 
-			$stmt = $conn->prepare(
-				"SELECT `albums`.*, `albums`.`artFilePath` AS 'artUrl', `albumDetails`.*
-				FROM `albums`
-				INNER JOIN `albumDetails` ON `albums`.`id` = `albumDetails`.`albumId`
-				WHERE `id` = :id"
-			);
-			$stmt->bindParam(":id", $albumId);
-			$stmt->execute();
-			$album = $stmt->fetch();
-		
-			if ($album === false) {
-				Flight::response()->status(404)->send();
-				return;
-			}
-		
-			$stmt = $conn->prepare(
-				"SELECT `songs`.*
-				FROM `songs`
-				INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
-				WHERE `song-album`.`albumId` = :albumId
-				ORDER BY `songs`.`discNumber`, `songs`.`trackNumber`"
-			);
-			$stmt->bindParam(":albumId", $albumId);
-			$stmt->execute();
-			$songs = $stmt->fetchAll();
-	
-			$albumArt = file_get_contents(substr($album['artFilepath'], 1));
+		if ($data === false) {
+			Flight::response()->status(404)->send();
+			return;
 		}
 
-		$album['englishTime'] = Utilities::secondsToEnglishTime($album['duration']);
-
-		foreach ($songs as &$song) {
+		foreach ($data['songs'] as &$song) {
 			$song['time'] = Utilities::secondsToTimeString($song['duration']);
 		}
+
+		$data['album']['englishTime'] = Utilities::secondsToEnglishTime($data['album']['duration']);
+		$data['accentColour'] = Utilities::getAccentColour($data['art']);
 	
-		$accentColour = Utilities::getAccentColour($albumArt);
+		$this->view('album', $data);
+	}
+
+	private function getDeezerAlbum($albumId) {
+		$albumId = str_replace(DeezerApi::DEEZER_ID_PREFIX, "", $albumId);
+		$filepath = "userData/deezer/album/$albumId";
+
+		if (!file_exists($filepath)) {
+			$deezerApi = new DeezerApi();
+			$res = $deezerApi->getAlbum($albumId);
+			file_put_contents($filepath, serialize($res));
+		} else {
+			$res = unserialize(file_get_contents($filepath));
+		}
+
+		return [
+			'album' => $res['album'],
+			'songs' => $res['songs'],
+			'art' => file_get_contents($res['album']['artUrl'])
+		];
+	}
+
+	private function getLocalAlbum($albumId) {
+		$db = new MusicDatabase();
+		$conn = $db->getConn();
+
+		$stmt = $conn->prepare(
+			"SELECT `albums`.*, `albums`.`artFilePath` AS 'artUrl', `albumDetails`.*
+			FROM `albums`
+			INNER JOIN `albumDetails` ON `albums`.`id` = `albumDetails`.`albumId`
+			WHERE `id` = :id"
+		);
+		$stmt->bindParam(":id", $albumId);
+		$stmt->execute();
+		$album = $stmt->fetch();
 	
-		$this->view('album', compact('album', 'songs', 'accentColour'));
+		if ($album === false) {
+			return false;
+		}
+	
+		$stmt = $conn->prepare(
+			"SELECT `songs`.*
+			FROM `songs`
+			INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
+			WHERE `song-album`.`albumId` = :albumId
+			ORDER BY `songs`.`discNumber`, `songs`.`trackNumber`"
+		);
+		$stmt->bindParam(":albumId", $albumId);
+		$stmt->execute();
+		$songs = $stmt->fetchAll();
+
+		$art = file_get_contents(substr($album['artFilepath'], 1));
+
+		return compact('album', 'songs', 'art');
 	}
 }
 
--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -10,120 +10,7 @@ use Flight;
 use PDO;
 
 class ApiController extends Controller {
-	public function song($songId) {
-		if (str_starts_with($songId, DeezerApi::DEEZER_ID_PREFIX)) {
-			$songId = str_replace(DeezerApi::DEEZER_ID_PREFIX, "", $songId);
-			$filepath = "userData/deezer/metadata/$songId";
-	
-			if (!file_exists($filepath)) {
-				$deezerPrivateApi = new DeezerPrivateApi();
-				$res = $deezerPrivateApi->getSongData($songId);
-				
-				file_put_contents($filepath, serialize($res));
-			} else {
-				$res = unserialize(file_get_contents($filepath));
-			}
-		} else {
-			$db = new MusicDatabase();
-			$conn = $db->getConn();
-			$stmt = $conn->prepare(
-				"SELECT
-					`songs`.`name` AS 'songName',
-					`songs`.`artist` AS 'songArtist',
-					`albums`.`artFilepath` AS 'albumArtUrl',
-					`albums`.`name` AS 'albumName',
-					`albums`.`id` AS 'albumId'
-				FROM `songs`
-				INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
-				INNER JOIN `albums` ON `song-album`.`albumId` = `albums`.`id`
-				WHERE `songs`.`id` = :id"
-			);
-			$stmt->bindParam(":id", $songId);
-			$stmt->execute();
-			$res = $stmt->fetch();	
-		}
-	
-		Flight::json($res);
-	}
-
-	public function album($albumId) {
-		if (str_starts_with($albumId, DeezerApi::DEEZER_ID_PREFIX)) {
-			$albumId = str_replace(DeezerApi::DEEZER_ID_PREFIX, "", $albumId);
-			$filepath = "userData/deezer/album/$albumId";
-	
-			if (!file_exists($filepath)) {
-				$deezerApi = new DeezerApi();
-				$res = $deezerApi->getAlbum($albumId);
-				
-				file_put_contents($filepath, serialize($res));
-			} else {
-				$res = unserialize(file_get_contents($filepath));
-			}
-	
-			$songIds = array_column($res['songs'], "id");
-		} else {
-			$db = new MusicDatabase();
-			$conn = $db->getConn();
-			$stmt = $conn->prepare(
-				"SELECT `songs`.`id`
-				FROM `songs`
-				INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
-				WHERE `song-album`.`albumId` = :albumId
-				ORDER BY `songs`.`discNumber`, `songs`.`trackNumber`"
-			);
-			$stmt->bindParam(":albumId", $albumId);
-			$stmt->execute();
-			$songIds = $stmt->fetchAll(PDO::FETCH_COLUMN);
-		}
-	
-		Flight::json(compact('songIds'));
-	}
-
-	public function search() {
-		$searchTerm = Flight::request()->query->term;
-
-		if (str_starts_with($searchTerm, "e: ")) {
-			$searchTerm = substr($searchTerm, strlen("e: "));
-	
-			$deezerApi = new DeezerApi();
-			$res = $deezerApi->search($searchTerm);
-	
-			$albums = $res['albums'];
-			$songs = $res['songs'];
-		} else {
-			$searchTerm = str_replace(" ", "%", $searchTerm);
-			$searchTerm = "%$searchTerm%";
-		
-			$db = new MusicDatabase();
-			$conn = $db->getConn();
-			$stmt = $conn->prepare(
-				"SELECT `id`, `name`, `artist`, `duration`, `albumDetails`.`duration`, `artFilepath`
-				FROM `albums`
-				INNER JOIN `albumDetails` ON `albums`.`id` = `albumDetails`.`albumId`
-				WHERE CONCAT(`name`, `artist`) LIKE :searchTerm
-				OR CONCAT(`artist`, `name`) LIKE :searchTerm
-				LIMIT 5"
-			);
-			$stmt->bindParam(":searchTerm", $searchTerm);
-			$stmt->execute();
-			$albums = $stmt->fetchAll();
-		
-			$stmt = $conn->prepare(
-				"SELECT `songs`.`id`, `songs`.`name`, `songs`.`artist`, `songs`.`duration`, `albums`.`artFilepath`, `albums`.`id` AS `albumId`
-				FROM `songs`
-				INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
-				INNER JOIN `albums` ON `song-album`.`albumId` = `albums`.`id`
-				WHERE CONCAT(`songs`.`name`, `songs`.`artist`) LIKE :searchTerm
-				OR CONCAT(`songs`.`artist`, `songs`.`name`) LIKE :searchTerm
-				LIMIT 5"
-			);
-			$stmt->bindParam(":searchTerm", $searchTerm);
-			$stmt->execute();
-			$songs = $stmt->fetchAll();
-		}
-	
-		Flight::json(compact('albums', 'songs'));
-	}
+	private const DEEZER_SEARCH_PREFIX = "e: ";
 
 	public function update() {
 		MusicManager::updateDatabase();
@@ -137,7 +24,6 @@ class ApiController extends Controller {
 			if (!file_exists($filepath)) {
 				$deezerPrivateApi = new DeezerPrivateApi();
 				$song = $deezerPrivateApi->getSong($songId);
-	
 				file_put_contents(
 					$filepath,
 					($song !== false) ? $song : ""
@@ -146,39 +32,179 @@ class ApiController extends Controller {
 		} else {
 			$db = new MusicDatabase();
 			$conn = $db->getConn();
+
 			$stmt = $conn->prepare("SELECT `filepath` FROM `songs` WHERE `id` = :id");
 			$stmt->bindParam(":id", $songId);
 			$stmt->execute();
 			$filepath = $stmt->fetchColumn();
 		}
-	
+
 		$filesize = filesize($filepath);
+		$startOffset = 0;
+		$endOffset = $filesize;
 	
 		if (isset($_SERVER['HTTP_RANGE'])) {
-			$bytes = explode(
+			list($startOffset, $endOffset) = explode(
 				"-",
 				str_replace("bytes=", "", $_SERVER['HTTP_RANGE'])
 			);
-			$startOffset = $bytes[0];
-			$endOffset = (!empty($bytes[1])) ? $bytes[1] : $filesize;
-		} else {
-			$startOffset = 0;
-			$endOffset = $filesize;
+
+			if (empty($endOffset)) {
+				$endOffset = $filesize;
+			}
 		}
-	
-		$file = fopen($filepath, 'r');
-		fseek($file, $startOffset);
-		$data = fread($file, $endOffset - $startOffset);
-		fclose($file);
 	
 		Flight::response()
 			->header('Accept-Ranges', 'bytes')
 			->header('Content-Type', 'audio/mpeg')
 			->header('Content-Range', "bytes $startOffset-" . ($endOffset - 1) . "/$filesize")
 			->status(206)
-			->write($data)
+			->write($this->getPartialMp3Data($filepath, $startOffset, $endOffset))
 			->send()
 		;
+	}
+
+	private function getPartialMp3Data($filepath, $startOffset, $endOffset) {	
+		$file = fopen($filepath, 'r');
+		fseek($file, $startOffset);
+		$data = fread($file, $endOffset - $startOffset);
+		fclose($file);
+
+		return $data;
+	}
+
+	public function song($songId) {
+		Flight::json(
+			str_starts_with($songId, DeezerApi::DEEZER_ID_PREFIX) ? $this->getDeezerSong($songId) : $this->getLocalSong($songId)
+		);
+	}
+
+	private function getDeezerSong($songId) {
+		$songId = str_replace(DeezerApi::DEEZER_ID_PREFIX, "", $songId);
+		$filepath = "userData/deezer/metadata/$songId";
+
+		if (!file_exists($filepath)) {
+			$deezerPrivateApi = new DeezerPrivateApi();
+			$song = $deezerPrivateApi->getSongData($songId);
+			file_put_contents($filepath, serialize($song));
+		} else {
+			$song = unserialize(file_get_contents($filepath));
+		}
+
+		return $song;
+	}
+
+	private function getLocalSong($songId) {
+		$db = new MusicDatabase();
+		$conn = $db->getConn();
+		
+		$stmt = $conn->prepare(
+			"SELECT
+				`songs`.`name` AS 'songName',
+				`songs`.`artist` AS 'songArtist',
+				`albums`.`artFilepath` AS 'albumArtUrl',
+				`albums`.`name` AS 'albumName',
+				`albums`.`id` AS 'albumId'
+			FROM `songs`
+			INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
+			INNER JOIN `albums` ON `song-album`.`albumId` = `albums`.`id`
+			WHERE `songs`.`id` = :id"
+		);
+		$stmt->bindParam(":id", $songId);
+		$stmt->execute();
+		$song = $stmt->fetch();
+
+		return $song;
+	}
+
+	public function album($albumId) {	
+		Flight::json(
+			str_starts_with($albumId, DeezerApi::DEEZER_ID_PREFIX) ? $this->getDeezerAlbum($albumId) : $this->getLocalAlbum($albumId)
+		);
+	}
+
+	private function getDeezerAlbum($albumId) {
+		$albumId = str_replace(DeezerApi::DEEZER_ID_PREFIX, "", $albumId);
+		$filepath = "userData/deezer/album/$albumId";
+
+		if (!file_exists($filepath)) {
+			$deezerApi = new DeezerApi();
+			$res = $deezerApi->getAlbum($albumId);
+			file_put_contents($filepath, serialize($res));
+		} else {
+			$res = unserialize(file_get_contents($filepath));
+		}
+
+		return ['songIds' => array_column($res['songs'], "id")];
+	}
+
+	private function getLocalAlbum($albumId) {
+		$db = new MusicDatabase();
+		$conn = $db->getConn();
+		$stmt = $conn->prepare(
+			"SELECT `songs`.`id`
+			FROM `songs`
+			INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
+			WHERE `song-album`.`albumId` = :albumId
+			ORDER BY `songs`.`discNumber`, `songs`.`trackNumber`"
+		);
+		$stmt->bindParam(":albumId", $albumId);
+		$stmt->execute();
+		$songIds = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+		return ['songIds' => $songIds];
+	}
+
+	public function search() {
+		$term = Flight::request()->query->term;
+	
+		Flight::json(
+			str_starts_with($term, self::DEEZER_SEARCH_PREFIX) ? $this->searchDeezer($term) : $this->searchLocal($term)
+		);
+	}
+
+	private function searchDeezer($term) {	
+		$deezerApi = new DeezerApi();
+		$res = $deezerApi->search(
+			substr($term, strlen(self::DEEZER_SEARCH_PREFIX))
+		);
+
+		return $res;
+	}
+
+	private function searchLocal($term) {
+		$term = str_replace(" ", "%", $term);
+		$term = "%$term%";
+	
+		$db = new MusicDatabase();
+		$conn = $db->getConn();
+
+		$stmt = $conn->prepare(
+			"SELECT `id`, `name`, `artist`, `duration`, `albumDetails`.`duration`, `artFilepath`
+			FROM `albums`
+			INNER JOIN `albumDetails` ON `albums`.`id` = `albumDetails`.`albumId`
+			WHERE CONCAT(`name`, `artist`) LIKE :term
+			OR CONCAT(`artist`, `name`) LIKE :term
+			LIMIT 5"
+		);
+		$stmt->bindParam(":term", $term);
+		$stmt->execute();
+		$albums = $stmt->fetchAll();
+	
+		$stmt = $conn->prepare(
+			"SELECT `songs`.`id`, `songs`.`name`, `songs`.`artist`, `songs`.`duration`, `albums`.`artFilepath`, `albums`.`id` AS `albumId`
+			FROM `songs`
+			INNER JOIN `song-album` ON `songs`.`id` = `song-album`.`songId`
+			INNER JOIN `albums` ON `song-album`.`albumId` = `albums`.`id`
+			WHERE CONCAT(`songs`.`name`, `songs`.`artist`) LIKE :term
+			OR CONCAT(`songs`.`artist`, `songs`.`name`) LIKE :term
+			LIMIT 5"
+		);
+		$stmt->bindParam(":term", $term);
+		$stmt->execute();
+		$songs = $stmt->fetchAll();
+
+		return compact('albums', 'songs');
 	}
 }
 
