ef9ef646632af8f7fc8a3706b287f3bcf899cb3a
Henry Li <henry_li@web.de>
2021-06-23 23:00:10 +0100

    Change route function

--- app/Route.php
+++ app/Route.php
@@ -7,8 +7,9 @@ use Flight;
 class Route {
 	public static function map($requestType, $uri, $callback) {
 		Flight::route("$requestType $uri", function(...$params) use ($callback) {
-			$class = "App\\Controllers\\" . $callback[0];
-			$method = $callback[1];
+			list($class, $method) = explode('@', $callback);
+
+			$class = "App\\Controllers\\$class";
 
 			$controller = new $class();
 			$controller->$method(...$params);
--- routes.php
+++ routes.php
@@ -2,15 +2,15 @@
 
 use App\Route;
 
-Route::map("GET", "/", [RootController::class, 'index']);
-Route::map("GET", "/queue", [RootController::class, 'queue']);
+Route::map("GET", "/", "RootController@index");
+Route::map("GET", "/queue", "RootController@queue");
 
-Route::map("GET", "/album/@albumId", [AlbumController::class, 'index']);
+Route::map("GET", "/album/@albumId", "AlbumController@index");
 
-Route::map("GET", "/api/mp3/@songId", [ApiController::class, 'mp3']);
-Route::map("GET", "/api/update", [ApiController::class, 'update']);
-Route::map("GET", "/api/search", [ApiController::class, 'search']);
-Route::map("GET", "/api/song/@songId", [ApiController::class, 'song']);
-Route::map("GET", "/api/album/@albumId", [ApiController::class, 'album']);
+Route::map("GET", "/api/mp3/@songId", "ApiController@mp3");
+Route::map("GET", "/api/update", "ApiController@update");
+Route::map("GET", "/api/search", "ApiController@search");
+Route::map("GET", "/api/song/@songId", "ApiController@song");
+Route::map("GET", "/api/album/@albumId", "ApiController@album");
 
 ?>
