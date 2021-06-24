ee8c2f66dc8e931be4083189164cb3589b0f601b
Henry Li <henry_li@web.de>
2021-06-24 19:33:28 +0100

    Update service workers
    Disable `chrome://flags/#check-offline-capability` to install as PWA

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,3 @@
-// self.addEventListener('activate', e => {
-// 	e.waitUntil(
-// 		caches.keys().then(cacheNames => {
-// 			return Promise.all(
-// 				cacheNames.map(cacheName => caches.delete(cacheName))
-// 			);
-// 	  	})
-// 	);
-// });
-
-// self.addEventListener('install', e => {
-// 	e.waitUntil(
-// 		caches.open('music-cache').then(cache => cache.addAll(['/'])),
-// 	);
-// });
-
-// self.addEventListener('fetch', e => {
-// 	e.respondWith(
-// 		caches.match(e.request).then(response => response || fetch(e.request)),
-// 	);
-// });
+self.addEventListener('activate', e => {});
+self.addEventListener('install', e => {});
+self.addEventListener('fetch', e => {});
