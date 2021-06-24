8a3c3315fc82ae13854d439efc703a1b2eb7f42f
Henry Li <henry_li@web.de>
2021-06-24 18:52:24 +0100

    SearchHandler refactor

--- public/js/SearchHandler.js
+++ public/js/SearchHandler.js
@@ -3,60 +3,62 @@ class SearchHandler {
 
 	constructor($searchBar, $clearSearchBarButton) {	
 		if (SearchHandler.sharedInstance) {
-			return;
-		} else {
 			SearchHandler.sharedInstance = this;
+		} else {
+			return;
 		}
 
-		this.$searchBar = $searchBar;
-		this.$clearSearchBarButton = $clearSearchBarButton;
+		this._$searchBar = $searchBar;
+		this._$clearSearchBarButton = $clearSearchBarButton;
 		
-		this.initEvents();
+		this._initEvents();
 	}
 
-	initEvents() {
+	_initEvents() {
 		$(window).mousedown(function(e) {
-			var activable = '[data-activable]';
-			var $element = $(e.target).is(activable) ? $(e.target) : $(e.target).parents(activable).first();
+			var $element = $(e.target).is(`[data-${SearchHandler.ACTIVABLE_DATA_SUFFIX}]`)
+				? $(e.target)
+				: $(e.target).parents(`[data-${SearchHandler.ACTIVABLE_DATA_SUFFIX}]`).first()
+			;
 
-			$(activable).removeClass('active');
+			$(`[data-${SearchHandler.ACTIVABLE_DATA_SUFFIX}]`).removeClass('active');
 
 			if ($element) {
 				$element.addClass('active');
 			}
 		});
-		this.$searchBar.keyup(e => this.search());
-		this.$clearSearchBarButton.click(e => this.reset());
+		this._$searchBar.keyup(e => this._search());
+		this._$clearSearchBarButton.click(e => this.reset());
 	}
 
-	search() {
-		clearTimeout(this.timeout);
+	_search() {
+		clearTimeout(this._timeout);
 
-		this.timeout = setTimeout(
+		this._timeout = setTimeout(
 			async() => {
-				var term = this.$searchBar.val();
+				var term = this._$searchBar.val();
 				var $searchResults = $('#searchResults');
 				var $songs = $('#songs');
 				var $albums = $('#albums');
 				var $songsContainer = $('#songsContainer');
 				var $albumsContainer = $('#albumsContainer');
 	
-				this.$searchBar.attr('value', term);
+				this._$searchBar.attr('value', term);
 	
 				if (term.trim() === "") {
-					this.$clearSearchBarButton.hide();
+					this._$clearSearchBarButton.hide();
 					$searchResults.hide();
 					$songs.hide();
 					$albums.hide();
 					return;
 				}
 	
-				this.$clearSearchBarButton.show();
+				this._$clearSearchBarButton.show();
 	
 				var res = await $.get('/api/search', { term: term });
 	
-				$songsContainer.empty().append(res.songs.map(song => this.createResultRow('song', song.id, song.albumId, song.name, song.artist, song.duration, song.artFilepath)));
-				$albumsContainer.empty().append(res.albums.map(album => this.createResultRow('album', album.id, album.id, album.name, album.artist, album.duration, album.artFilepath)));
+				$songsContainer.empty().append(res.songs.map(song => this._createResultRow('song', song.id, song.albumId, song.name, song.artist, song.duration, song.artFilepath)));
+				$albumsContainer.empty().append(res.albums.map(album => this._createResultRow('album', album.id, album.id, album.name, album.artist, album.duration, album.artFilepath)));
 	
 				(res.songs.length > 0 || res.albums.length > 0) ? $searchResults.show() : $searchResults.hide();
 				(res.songs.length > 0) ? $songs.show(): $songs.hide();
@@ -68,7 +70,7 @@ class SearchHandler {
 		);
 	}
 
-	createResultRow(type, id, albumId, name, artist, duration, artFilepath) {
+	_createResultRow(type, id, albumId, name, artist, duration, artFilepath) {
 		var $resultRow = $(`<div class="result-row" data-${type}-id=${id} data-album-id=${albumId} data-context-menu-actions="QUEUE,GO_TO_ALBUM" data-activable></div>`);
 		var $img = $('<img>').prop('src', artFilepath);
 		var $artwork = $('<div class="artwork"></div>').append($img);
@@ -78,7 +80,7 @@ class SearchHandler {
 		]);
 		var $totalTime = (duration) ? $('<div class="total-time"></div>').text(secondsToTimeString(duration)) : "";
 
-		$resultRow.dblclick(() => (type === 'song') ? this.playSong($resultRow) : this.playAlbum($resultRow));
+		$resultRow.dblclick(() => (type === 'song') ? this._playSong($resultRow) : this._playAlbum($resultRow));
 		$resultRow.append([
 			$('<div></div>').append([$artwork, $details]),
 			$totalTime
@@ -87,11 +89,11 @@ class SearchHandler {
 		return $resultRow;
 	}
 
-	playSong($resultRow) {
+	_playSong($resultRow) {
 		MusicControl.sharedInstance.music().changeSong($resultRow.data('song-id'), true);
 	}
 
-	async playAlbum($resultRow) {
+	async _playAlbum($resultRow) {
 		var res = await $.get(`/api/album/${$resultRow.data('album-id')}`);
 		MusicControl.sharedInstance.music().playNextUp({
 			list: res.songIds,
@@ -100,7 +102,11 @@ class SearchHandler {
 	}
 
 	reset() {
-		this.$searchBar.val(null);
-		this.search();
+		this._$searchBar.val(null);
+		this._search();
+	}
+
+	static get ACTIVABLE_DATA_SUFFIX() {
+		return 'activable';
 	}
 }
