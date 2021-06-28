f8590587ac922b5d99806aa8b9c09720a1d1c6ee
Henry Li <henry_li@web.de>
2021-06-28 02:01:10 +0100

    Update DB

--- database.sql
+++ database.sql
@@ -62,19 +62,6 @@ CREATE TABLE `deezerSavedSongs` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Temporary table structure for view `deezerSavedSongs-noPrefix`
---
-
-DROP TABLE IF EXISTS `deezerSavedSongs-noPrefix`;
-/*!50001 DROP VIEW IF EXISTS `deezerSavedSongs-noPrefix`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `deezerSavedSongs-noPrefix` AS SELECT 
- 1 AS `songId`,
- 1 AS `flagged`*/;
-SET character_set_client = @saved_cs_client;
-
---
 -- Table structure for table `song-album`
 --
 
@@ -129,24 +116,6 @@ CREATE TABLE `songs` (
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `deezerSavedSongs-noPrefix`
---
-
-/*!50001 DROP VIEW IF EXISTS `deezerSavedSongs-noPrefix`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `deezerSavedSongs-noPrefix` AS select replace(`deezerSavedSongs`.`songId`,'DEEZER-','') AS `songId`,`deezerSavedSongs`.`flagged` AS `flagged` from `deezerSavedSongs` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -157,4 +126,4 @@ CREATE TABLE `songs` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-27 21:45:46
+-- Dump completed on 2021-06-28  2:00:38
