e58f9f6a5491cc924aea49350fe1eb49f8b0e64d
Henry Li <henry_li@web.de>
2021-09-06 21:37:22 +0100

    CSS

--- views/windowControlsOverlay.php
+++ views/windowControlsOverlay.php
@@ -1,5 +1,5 @@
 <div id="windowControlsOverlay">
-	<div style="<?= ($os['os_family'] === 'macintosh') ? 'margin-left: 4rem;' : null ?>">
+	<div style="<?= ($os['os_family'] === 'macintosh') ? 'margin-left: 5rem;' : null ?>">
 		<i id="searchIcon" class="fal fa-search"></i>
 		<input type="text" id="searchBar" placeholder="Search" spellcheck="false">
 		<i id="clearSearchBarButton" class="fal fa-times" style="display: none;"></i>
