829e283ea00a8365d8625543e3a4d5a021e809ea
Henry Li <henry_li@web.de>
2022-04-26 14:53:27 +0100

    Remove log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/service-worker.js');
 	navigator.serviceWorker.addEventListener('message', e => {
-		console.log(e);
 		switch (e.data) {
 			case 'previous-action':
 				Music.sharedInstance.previous(true);
