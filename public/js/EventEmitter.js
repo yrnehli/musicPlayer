db1a2119a7fd98de1ee55b44b17464c9da9e4807
Henry Li <henry_li@web.de>
2022-05-03 14:46:06 +0100

    Bug fix

--- public/js/EventEmitter.js
+++ public/js/EventEmitter.js
@@ -32,7 +32,7 @@ class EventEmitter {
             var [event, namespace] = event.split(".");
         }
 
-        var events = self['_on' + event];
+        var events = self['_on' + event] || [];
 
         if (fn || namespace) { // Loop through event store and remove the passed function.
             for (var i = 0; i < events.length; i++) {
@@ -59,15 +59,11 @@ class EventEmitter {
 
     _emit(event, id, msg) {
         var self = this;
-        var events = self['_on' + event];
-
-        if (! events) {
-            return self;
-        }
+        var events = self['_on' + event] || [];
 
         // Loop through event store and fire all functions.
         for (var i = events.length - 1; i >= 0; i--) { // Only fire the listener if the correct ID is used.
-            if (! events[i].id || events[i].id === id || event === 'load') {
+            if (!events[i].id || events[i].id === id || event === 'load') {
                 setTimeout(function (fn) {
                     fn.call(this, id, msg);
                 }.bind(self, events[i].fn), 0);
