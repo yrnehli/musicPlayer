639cb1a585b2c8f804ecba2f5cd3c1aa5d921c9f
Henry Li <henry_li@web.de>
2022-10-30 15:15:10 +0000

    Remove integrity checks

--- views/shell.php
+++ views/shell.php
@@ -2,11 +2,11 @@
 <html>
 	<head>
 		<meta name="theme-color" content="#121212">
-		<link rel="stylesheet" href="/public/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-		<script src="/public/js/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-		<script src="/public/js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="/public/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-		<script src="/public/js/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="/public/css/bootstrap.min.css" crossorigin="anonymous">
+		<script src="/public/js/jquery-3.4.1.js" crossorigin="anonymous"></script>
+		<script src="/public/js/popper.min.js" crossorigin="anonymous"></script>
+		<script src="/public/js/bootstrap.min.js" crossorigin="anonymous"></script>
+		<script src="/public/js/jquery-ui.js" crossorigin="anonymous"></script>
 		<script src="/public/js/simplebar.min.js"></script>
 		<script src="/public/js/jquery.waitforimages.min.js"></script>
 		<script src="/public/js/lazyload.min.js"></script>
